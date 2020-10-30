899be9254c08d9af9885d6360d75518239ae2373
Armando-Alamilla <armando_alamilla10@hotmail.com>
2020-10-30 12:03:38 -0600

    Ejemplo de encapsulamiento y acceso a un atributo con un metodo get

---
+++ Escuela/Alumno.java
@@ -0,0 +1,13 @@
+package Escuela;
+
+public class Alumno {
+  private int matricula = 4123;
+
+  public int getMatricula() {
+    return this.matricula;
+  }
+
+  public void saludar() {
+    System.out.println("Hola, soy el Alumno");
+  }
+}
---
+++ Escuela/Escuela.java
@@ -0,0 +1,10 @@
+package Escuela;
+
+public class Escuela {
+  public static void main(String[] args) {
+    Alumno alumno = new Alumno();
+    alumno.saludar();
+
+    System.out.println(alumno.getMatricula());
+  }
+}
